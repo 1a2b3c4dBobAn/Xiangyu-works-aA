5d880ea86915f48c2815e7ba8ded204870e320c5
Xiangyu AN <1a2b3c4d@berkeley.edu>
2018-05-01 08:41:47 -0700

    SQL Play playwright

--- W2D5/LRU_Cache.rb
+++ W2D5/LRU_Cache.rb
@@ -1,36 +1,53 @@
-class LRUCache
+# class LRUCache
+#
+#   def initialize(size)
+#     @size = size
+#     @cache = []
+#   end
+#
+#   def count
+#     @cache.count
+#     # returns number of elements currently in cache
+#   end
+#
+#   def add(el)
+#     # adds element to cache according to LRU principle
+#     if @cache.include?(el)
+#       @cache.delete(el)
+#       @cache << el
+#     elsif count >= @size
+#       @cache.shift
+#       @cache << el
+#     else
+#       @cache << el
+#     end
+#   end
+#
+#   def show
+#     p @cache
+#     nil
+#     # shows the items in the cache, with the LRU item first
+#   end
+#
+#   private
+#   # helper methods go here!
+#
+#
+# end
 
+
+class Store
   def initialize(size)
     @size = size
-    @cache = []
+    @store = Array.new(size)
   end
 
-  def count
-    @cache.count
-    # returns number of elements currently in cache
+  def set
+
+    #will call resize here
   end
 
-  def add(el)
-    # adds element to cache according to LRU principle
-    if @cache.include?(el)
-      @cache.delete(el)
-      @cache << el
-    elsif count >= @size
-      @cache.shift
-      @cache << el
-    else
-      @cache << el
-    end
+  def resize
+
   end
-
-  def show
-    p @cache
-    nil
-    # shows the items in the cache, with the LRU item first
-  end
-
-  private
-  # helper methods go here!
-
-
 end
